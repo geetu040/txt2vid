de9d1dca617dc50d86b959ba16025e968db62ccc
computer <raoarmaghanshakir040@gmail.com>
2023-10-13 20:35:24 +0500

    added option for file extension

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from pathlib import Path
 from generator import generate_video
+from video_convertor import convert_video_format
 
 app = FastAPI()
 
 @app.get("/")
 async def index():
-    return "Hi"
+	print("someone just went to the home page")
+	return "Hi"
 
 @app.get("/get_video")
-async def get_video(prompt):
+async def get_video(prompt, video_format="mp3"):
 	video_path = generate_video(prompt)
+	if video_format in ["webm", "mov"]:
+		video_path = convert_video_format(video_path, video_format)
 	return FileResponse(video_path, media_type="video/mp4")
---
+++ video_convertor.py
@@ -0,0 +1,25 @@
+import os
+from moviepy.editor import VideoFileClip
+
+def convert_video_format(input_video_path, output_format):
+	output_format = "." + output_format
+
+	# Load the video
+	video_clip = VideoFileClip(input_video_path)
+
+	# Extract the original file path and name without extension
+	video_path, video_name = os.path.split(input_video_path)
+	video_name_without_ext, _ = os.path.splitext(video_name)
+
+	# Define the output video file path with the new extension
+	output_video_path = os.path.join(video_path, video_name_without_ext + output_format)
+
+	video_codec = 'libvpx' if output_format == '.webm' else 'libx264'
+
+	# Convert the video to the specified format
+	video_clip.write_videofile(output_video_path, codec=video_codec)
+
+	print(f"Video converted and saved as {output_video_path}")
+
+if __name__ == "__main__":
+	convert_video_format("test\\thunder-file_e46c3db6.mp4", "webm")
