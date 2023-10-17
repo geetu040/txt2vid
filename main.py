5720378f740c5bd828368e96d1f55627bbb58826
computer <raoarmaghanshakir040@gmail.com>
2023-10-17 20:02:44 +0500

    added dummy video

--- main.py
+++ main.py
@@ -16,6 +16,28 @@ async def index():
 	logging.info('Hit at Index')
 	return "The App is running"
 
+@app.get("/get_dummy_video")
+async def get_dummy_video(prompt, video_format="mp3", video_length=1):
+	logging.info(f'prompt: {prompt}\tvideo_format: {video_format}\tvideo_length:{video_length}')
+
+	# making sure video_length is between 1 and 10 seconds
+	video_length = int(video_length)
+	video_length = min(video_length, 15)
+	video_length = max(video_length, 1)
+
+	# video_path = generate_video(prompt, video_length)
+	video_path = "dummy_video.mp4"
+
+	if video_format in ["webm", "mov"]:
+		video_path = convert_video_format(video_path, video_format)
+
+	media_type, _ = mimetypes.guess_type(video_path)
+
+	print("Media Type:", media_type)
+	logging.info(f'video_path: {video_path}\tmedia_type: {media_type}')
+
+	return FileResponse(video_path, media_type=media_type)
+
 @app.get("/get_video")
 async def get_video(prompt, video_format="mp3", video_length=1):
 	logging.info(f'prompt: {prompt}\tvideo_format: {video_format}\tvideo_length:{video_length}')
