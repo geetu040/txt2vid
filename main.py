6d852c325e0424e510ce8f1071a1ba275312bf01
computer <raoarmaghanshakir040@gmail.com>
2023-10-23 13:59:30 +0500

    formating log file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from generator import generate_video
 from video_convertor import convert_video_format
 import logging
+import time
 
 logging.basicConfig(filename='app_logs.log', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
@@ -37,6 +38,7 @@ async def get_dummy_video(prompt, video_format="mp3", video_length=1):
 
 @app.get("/get_video")
 async def get_video(prompt, video_format="mp3", video_length=1):
+	logging.info('')
 	logging.info(f'prompt: {prompt}\tvideo_format: {video_format}\tvideo_length:{video_length}')
 
 	# making sure video_length is between 1 and 10 seconds
@@ -45,13 +47,18 @@ async def get_video(prompt, video_format="mp3", video_length=1):
 	video_length = max(video_length, 1)
 
 	# video_path = "test\\thunder-file_e46c3db6.mp4"
+	time_i = time.time()
 	video_path = generate_video(prompt, video_length)
+	time_f = time.time()
+	logging.info(f'Time: {round(time_f - time_i, 2)}')
+
+
 	if video_format in ["webm", "mov"]:
 		video_path = convert_video_format(video_path, video_format)
 
 	media_type, _ = mimetypes.guess_type(video_path)
 
-	print("Media Type:", media_type)
 	logging.info(f'video_path: {video_path}\tmedia_type: {media_type}')
+	logging.info('')
 
 	return FileResponse(video_path, media_type=media_type)
